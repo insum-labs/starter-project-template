e452949da482208f4aca14274c4fce61620b2c6b
Martin D'Souza <martin@clarifit.com>
2020-06-09 08:50:24 -0600

    #2: minor comments

--- .vscode/scripts/run_sql.sh
+++ .vscode/scripts/run_sql.sh
@@ -16,6 +16,7 @@ if [ -z "$VSCODE_TASK_COMPILE_FILE" ]; then
   echo -e "Defaulting to full path"
   VSCODE_TASK_COMPILE_FILE=$FILE_FULL_PATH
 fi
+# Since VSCODE_TASK_COMPILE_FILE contains the variable reference need to evaluate it here
 VSCODE_TASK_COMPILE_FILE=$(eval "printf $VSCODE_TASK_COMPILE_FILE")
 
 echo -e "Parsing file: ${COLOR_LIGHT_GREEN}$VSCODE_TASK_COMPILE_FILE${COLOR_RESET}"
