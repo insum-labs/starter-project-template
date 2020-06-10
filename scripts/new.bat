359b63a900808dc526d7cc04fb73281a30624927
Martin D'Souza <martin@clarifit.com>
2020-06-10 16:44:23 -0600

    #14 Remove new.sh scripts as no longer necessary

--- scripts/new.bat
+++
@@ -1,9 +0,0 @@
-
-rem This script will clean the release script and start a new template
-
-echo "Start new release"
-
-rem Does that work in Windows?
-del ../release/[a-z]*.sql
-
-copy ../release/_release_template.sql ../release/_release.sql
--- scripts/new.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script will clean the release script and start a new template
-echo "Start new release"
-rm ../release/[a-z]*.sql
-cp ../release/_release_template.sql ../release/_release.sql
