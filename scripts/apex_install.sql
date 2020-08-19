ccce2ebd72e7b8cfda1fb243e2f340e13afa1e6f
Martin D'Souza <martin@clarifit.com>
2020-08-19 10:21:09 -0600

    Use apex_install.set_workspace in scripts/apex_install.sql #32

--- scripts/apex_install.sql
+++ scripts/apex_install.sql
@@ -9,17 +9,11 @@ set serveroutput on size unlimited;
 set timing off;
 
 declare
-  l_workspace_id apex_workspaces.workspace_id%type;
-  l_build_option_id apex_application_build_options.build_option_id%type;
 begin
-  select workspace_id
-  into l_workspace_id
-  from apex_workspaces
-  where workspace = upper('&2.');
 
   apex_application_install.set_application_id(&3.);
-  apex_application_install.set_schema('&1.');
-  apex_application_install.set_workspace_id(l_workspace_id);
+  apex_application_install.set_schema(upper('&1.'));
+  apex_application_install.set_workspace(upper('&2.'));
   apex_application_install.generate_offset;
 
 end;
